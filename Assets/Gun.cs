2f9eba0c6f66bcfbbf52c2ab5a40b534d791826f
Nikolay Bezdetniy <32260582+Azonos712@users.noreply.github.com>
2020-10-16 21:33:10 +0300

    Add gun and bullets, logic of gun shot

---
+++ Assets/Gun.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public int Bullets;
+    public GameObject BulletPrefab;
+    public GameObject SpawnPoint;
+    public int BulletSpeed = 80;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Bullets > 0)
+            {
+                Bullets = Bullets - 1;
+                GameObject newBullet = Instantiate(BulletPrefab, SpawnPoint.transform.position, SpawnPoint.transform.rotation);
+                newBullet.GetComponent<Rigidbody>().velocity = SpawnPoint.transform.forward * BulletSpeed;
+            }
+            else
+                Debug.Log("Нет пуль!!!");
+        }
+    }
+}
